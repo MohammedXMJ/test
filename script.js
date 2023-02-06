b40856cc62f4d82ff4ee30682a4f63c984b14b0a
Mohammed Alzuabidi <122676805+MohammedXMJ@users.noreply.github.com>
2023-02-06 10:25:18 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+// $(".login").hide();
+// $(".signup").hide();
+
+
+// $(".loginbtn").click(function(){
+//     $(".login").show();
+//     $(".signup").hide();
+// })
+
+// $(".signupntn").click(function(){
+//     $(".signup").show();
+//     $(".login").hide();
+// })
+
+
